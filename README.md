6ded90fac6f565b61a49a9ad3bada91e6a210afc
DoctorFeelGoodMD <82419179+DoctorFeelGoodMD@users.noreply.github.com>
2021-04-12 19:07:03 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# OxygenNotIncluded-Mods
+ 
---
+++ source/DebrisMeltsToDebris/DebrisMeltsToDebris_Patch.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Harmony;
+using UnityEngine;
+
+namespace DebrisMeltsToDebris
+{
+    [HarmonyPatch(typeof(SimTemperatureTransfer), "DoStateTransition")]
+    public class DebrisMeltsToDebris_Patches
+    {
+        static bool Prefix(int sim_handle, ref Dictionary<int,SimTemperatureTransfer> ___handleInstanceMap)
+        {
+
+            SimTemperatureTransfer value = null;
+            if (!___handleInstanceMap.TryGetValue(sim_handle, out value) || value == null || value.HasTag(GameTags.Sealed))
+            {
+                return false; //wow that three underscores thing worked, holy cow I am not a good programmer
+            }
+            Pickupable pickupable = value.GetComponent<Pickupable>();
+            if (!pickupable)
+            {
+                return true;
+            }
+            Element element = pickupable.PrimaryElement.Element;
+            if (element.IsSolid)
+            {
+
+                PrimaryElement primaryElement = pickupable.PrimaryElement;
+                Element newElement = ElementLoader.elements[SimMessages.GetElementIndex(element.highTempTransitionTarget)];
+
+                if (newElement.IsSolid) //new and old elements both solid for this to work (I think this is default condition of DoStateTransition...but idk)
+                {
+                    GameObject gameObject = newElement.substance.SpawnResource(pickupable.transform.GetPosition(), pickupable.PrimaryElement.Mass, primaryElement.Temperature, primaryElement.DiseaseIdx, primaryElement.DiseaseCount);
+                    if (gameObject.GetComponent<Pickupable>() != null && WorldInventory.Instance.IsReachable(gameObject.GetComponent<Pickupable>()))
+                    {
+                        PopFXManager.Instance.SpawnFX(PopFXManager.Instance.sprite_Resource, Mathf.RoundToInt(primaryElement.Mass) + " " + newElement.name, gameObject.transform);
+                    }   // spawns a resource the same way one does when you mine it (WorldDamage.OnDigComplete)
+
+                }
+                value.OnDestroy();
+                Util.KDestroyGameObject(value.gameObject);
+                return false;
+            }
+            return true;
+        }
+    }
+}

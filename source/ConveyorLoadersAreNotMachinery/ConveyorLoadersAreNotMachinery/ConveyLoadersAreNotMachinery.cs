2f6560381cb53025d47f93234bce3ac282cc95a8
DoctorFeelGoodMD <82419179+DoctorFeelGoodMD@users.noreply.github.com>
2021-08-24 17:39:52 -0400

    Uploaded

---
+++ source/ConveyorLoadersAreNotMachinery/ConveyorLoadersAreNotMachinery/ConveyLoadersAreNotMachinery.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HarmonyLib;
+using UnityEngine;
+
+
+namespace ConveyorLoadersAreNotMachinery
+{
+	public class ConveyorLoadersAreNotMachinery_Patches : KMod.UserMod2
+	{
+
+
+		[HarmonyPatch(typeof(SolidConduitInboxConfig), "DoPostConfigureComplete")]
+		public class ConveyorLoadersAreNotMachinery_Patches_DoPostConfigureComplete
+		{
+			static void Postfix(GameObject go)
+			{
+				go.RemoveTag(RoomConstraints.ConstraintTags.IndustrialMachinery);
+			}
+		}
+	}
+}
+	
---
+++ source/ConveyorLoadersAreNotMachinery/readme.md
@@ -0,0 +1,7 @@
+This mod makes it so that Conveyor Loaders don't count as "industrial machinery" for the purposes of mess hall and great hall designations.
+
+Dupes constantly drop food on the floor, and it will often not be picked up for quite some time if not for sweepers. If your refrigeration system is somewhere away from your mess hall (somewhere -18C for example) it is difficult to prevent spoilage without a conveyor loader. This makes your dupes a bit more understanding.
+
+Does not affect any other machines.
+
+Should work with DLC.

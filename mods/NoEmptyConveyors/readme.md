85f00aa3b916e168685a10ced14f76d9d8d4579e
DoctorFeelGoodMD <82419179+DoctorFeelGoodMD@users.noreply.github.com>
2021-08-29 18:41:30 -0400

    uploaded NoEmptyConveyors

---
+++ mods/NoEmptyConveyors/readme.md
@@ -0,0 +1,9 @@
+This mod makes solids that melt on the conveyor rail system no longer leave behind empty baskets. 
+
+These empty conveyor baskets cannot be read by automation sensors, but will still occupy space in the network and can clog up automated rail systems that handle meltable objects.
+
+Objects will still melt and sublime on the conveyor rails normally, the mod only changes the fact that the baskets they used to occupy will disappear allowing the baskets behind to continue moving forward.
+
+This mod works very well with my other debris transition mod, Debris Melts to Debris. However, this mod must be first in the mod loading order (this mod above DMTD on the mods list on the main menu) With these two mods you will be able to create some very complex boiler, freezer, and forge designs and take advantage of the high mass and thermal throughput of solid rails.
+
+Please report any bugs!
---
+++ source/NoEmptyConveyors/NoEmptyConveyors/NoEmptyConveyorsPatch.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HarmonyLib;
+using UnityEngine;
+
+namespace NoEmptyConveyors
+{
+    [HarmonyPatch(typeof(SimTemperatureTransfer), "DoOreMeltTransition")]
+    //this mod must be loaded before Debris Melts to Debris as they both prefix DoOreMeltTransition
+
+    public class NoEmptyConveyorsPatch : KMod.UserMod2
+    {
+
+        static bool Prefix(int sim_handle, ref Dictionary<int, SimTemperatureTransfer> ___handleInstanceMap, out int __state)
+        {
+            SimTemperatureTransfer value = null;
+            __state = new int();
+            __state = 0;
+            if (!___handleInstanceMap.TryGetValue(sim_handle, out value) || value == null || value.HasTag(GameTags.Sealed))
+            {
+                return false;
+            }
+            Pickupable pickupable = value.GetComponent<Pickupable>();
+            if (!pickupable)
+            {
+                return true;
+            }
+            __state = Grid.PosToCell(pickupable.transform.GetPosition());
+            if (!Game.Instance.solidConduitFlow.HasConduit(__state))
+            { __state = 0; }
+            return true;
+            //if an object is melting and is a pickable in a cell with a solid conduit, prefix passes the cell ID to postfix
+            //unfortunately the pickupable object itself does not know it is on a conduit
+            //otherwise it sends zero
+            //must pass location to postfix like this because DoOreMeltTransition destroys the pickupable gameobject
+        }
+        static void Postfix(int __state)
+        {
+            if (__state <= 0)
+            {
+                return; //skip everything if not pickupable or on conduit square
+            }
+            List<int> positions = new List<int>();
+            positions.Add(__state);
+            positions.Add(Grid.CellAbove(__state));
+            positions.Add(Grid.CellBelow(__state));
+            positions.Add(Grid.CellLeft(__state)); //check neighboring cells just to be safe, conduit movement is super odd with pickupables
+            positions.Add(Grid.CellRight(__state)); //the conduit considers it to be in the next square, but the pickupable itself lags behind
+
+            SolidConduitFlow solidConduitFlow = Game.Instance.solidConduitFlow;
+
+            foreach (var i in positions)
+            {
+                if (!solidConduitFlow.HasConduit(i))
+                { continue; } //skip neighboring cells without conduits
+
+                SolidConduitFlow.Conduit conduit = solidConduitFlow.GetConduit(i);
+                SolidConduitFlow.ConduitContents contents;
+                contents = solidConduitFlow.GetContents(conduit.GetCell(solidConduitFlow)); //check contents of each conduit in each 5 squares
+
+                if (contents.pickupableHandle.IsValid()) //this skips empty conduits
+                {
+                    Pickupable pickupable = solidConduitFlow.GetPickupable(contents.pickupableHandle);
+                    if (!pickupable.isActiveAndEnabled) //for some reason 'IsNullOrDestroyed()' works only for neighboring cells but not its own
+                    {
+                        solidConduitFlow.EmptyConduit(conduit.GetCell(solidConduitFlow)); //there are a few functions which remove conduit contents, this one seems right
+                        
+                    }
+                }
+
+
+            }
+            //before this mod is applied, empty conveyor baskets still have all the data (mass, element, etc) of what used to be inside, so I really think this is a bug fix
+            //the conveyor system doesn't realize it is empty because when a pickupable is removed by melting this information is not passed to SolidConduitFlow
+        }
+
+    }
+}

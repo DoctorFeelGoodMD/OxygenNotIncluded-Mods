13b3722532f77633e05644bf8c8785cc62e3563f
DoctorFeelGoodMD <82419179+DoctorFeelGoodMD@users.noreply.github.com>
2021-09-14 14:38:50 -0400

    upload petrol mod

---
+++ source/PetroleumDoesntBreakPipes/PetroleumDoesntBreakPipes_Patch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HarmonyLib;
+using UnityEngine;
+
+namespace PetroleumDoesntBreakPipes
+{
+    [HarmonyPatch(typeof(ConduitFlow), "MeltConduitContents")] //"DoStateTransition")], old name prior to harmony 2.0 update
+    public class PetroleumDoesntBreakPipes_Patch : KMod.UserMod2
+    {
+        static bool Prefix(int conduit_idx, ConduitFlow __instance)
+        {
+            if (__instance.soaInfo.GetConduit(conduit_idx).GetContents(__instance).element != SimHashes.CrudeOil)
+            {
+                return true; //not crude oil, evaporate and damage pipes as usual
+            }
+            ConduitFlow.ConduitContents contents = __instance.soaInfo.GetConduit(conduit_idx).GetContents(__instance);
+            contents.element = SimHashes.Petroleum; //set new pipe contents to petrol
+            __instance.soaInfo.GetConduit(conduit_idx).SetContents(__instance, contents);
+
+            return false; //skip original method which breaks pipe and dumps on floor
+        }
+    }
+    
+}
---
+++ source/PetroleumDoesntBreakPipes/readme.md
@@ -0,0 +1,7 @@
+This mod makes liquid pipes no longer break if Crude Oil turns to Petroleum inside.
+
+Crude Oil turns to Petroleum around 400C, if this occurs while the Crude Oil is in a liquid pipe it will damage the pipe and the Petroleum will leak out. This does not make sense because Crude Oil and Petroleum are both liquids. Petroleum boiler designs are further complicated by difficulty pumping the hot liquids back up without overheating the pumps, which mean efficient counterflow designs usually require huge tile areas and/or exploits to work properly.
+
+This mod makes it so that Crude Oil heated to 400C will turn into Petroleum and remain the pipe, with no leaks or damage. This does not affect any other transitions. Pipes will still be damaged by liquids turning to solid or gas, including Sour Gas. 
+
+Please report any bugs!

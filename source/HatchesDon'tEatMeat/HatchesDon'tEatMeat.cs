747a546a9a34fcd0a9985586fefbd85e85ff49a7
DoctorFeelGoodMD <82419179+DoctorFeelGoodMD@users.noreply.github.com>
2021-11-13 21:44:43 -0500

    new hatch diet mod

---
+++ source/HatchesDon'tEatMeat/HatchesDon'tEatMeat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HarmonyLib;
+using UnityEngine;
+
+namespace HatchesDontEatMeat
+{
+    [HarmonyPatch(typeof(BaseHatchConfig),"FoodDiet")]
+    public class HatchesDontEatMeat_Patch : KMod.UserMod2
+    {
+        static void Postfix(ref List<Diet.Info> __result)
+        {
+
+            for (int i = 0; i < __result.Count; i++) //iterates through list of items on diet list 'fooddiet' which includes non-mineral foods
+            {
+                if (__result[i].consumedTags.First().ToString() == "Meat") //this tag system is pretty confusing
+                {
+                    __result.RemoveAt(i);
+                }
+                
+            }
+        }
+    }
+}
---
+++ source/HatchesDon'tEatMeat/readme.md
@@ -0,0 +1,5 @@
+This mod removes raw meat from the diet of hatches and their variants. 
+
+This prevents the problem of hatches immediately eating the meat from other hatches who die or are killed in non-mechanized ranches. This is particularly intended for early game before auto-sweepers are available.
+
+Affects only raw non-fish meat. They will still eat barbeque, if you really want to do that for some reason.

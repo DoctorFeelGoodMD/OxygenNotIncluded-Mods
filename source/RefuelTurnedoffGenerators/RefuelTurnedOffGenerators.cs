91635f2112f53e788a6fa62eb68bc01c215325db
DoctorFeelGoodMD <82419179+DoctorFeelGoodMD@users.noreply.github.com>
2021-11-15 07:10:48 -0500

    Added refueling mod

---
+++ source/RefuelTurnedoffGenerators/RefuelTurnedOffGenerators.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Refuel_Turned_off_Generators
+{
+
+    public class RefuelTurnedOffGenerators : KMod.UserMod2
+    {
+        [HarmonyPatch(typeof(GeneratorConfig), "ConfigureBuildingTemplate")]
+        public static class RefuelTurnedOffCoalGenerators_Patch
+        {
+            static void Postfix(GameObject go, Tag prefab_tag)
+            {
+                ManualDeliveryKG manualDeliveryKG = go.GetComponent<ManualDeliveryKG>();
+                manualDeliveryKG.operationalRequirement = FetchOrder2.OperationalRequirement.None; //I'll just change this to 'none', should be fine right?
+            }
+        }
+
+        [HarmonyPatch(typeof(WoodGasGeneratorConfig), "DoPostConfigureComplete")]
+        public static class RefuelTurnedOffWoodGenerators_Patch
+        {
+            static void Postfix(GameObject go)
+            {
+                ManualDeliveryKG manualDeliveryKG = go.GetComponent<ManualDeliveryKG>();
+                manualDeliveryKG.operationalRequirement = FetchOrder2.OperationalRequirement.None;
+            }
+        }
+    }
+}
---
+++ source/RefuelTurnedoffGenerators/readme.md
@@ -0,0 +1,5 @@
+This mod allows generators to be manually refilled with coal even when they are turned off by automation system.
+
+Generators normally only request fuel when they are actually running, and cancel the refuelling task if they are turned off. This can be problematic if they are turned on and off frequently by smart batteries, but auto-sweepers are not available yet. Dupes often haven't arrived to the generator by the time it gets turned back off even with a high job priority. This mod allows generators to always be refilled.
+
+This behavior also applies to wood generators.

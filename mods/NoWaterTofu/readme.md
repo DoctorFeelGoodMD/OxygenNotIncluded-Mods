108de1f06bc80ee25ed8a1be1c6313ab91a4c797
DoctorFeelGoodMD <82419179+DoctorFeelGoodMD@users.noreply.github.com>
2021-11-20 14:55:50 -0500

    nosh bean mod

---
+++ mods/NoWaterTofu/readme.md
@@ -0,0 +1,5 @@
+This mod removes water from the Tofu recipe. It now only requires 6 Nosh Beans.
+
+Nosh Beans are almost never used because they are so difficult to grow. The requirement then for water to make Tofu is particularly ridiculous because it requires a pitcher pump to be used, which feels like a kitchen-design annoyance rather than a challenge. Compared to Sleet Wheat, which not only lacks this requirement to make bread but can make high-end products with only a single cooking step, beans and tofu products have almost no advantages.
+
+This no-water recipe aims to bring Nosh Beans a little closer to the alternatives when considering a routine food supply.
---
+++ source/NoWaterTofu/NoWaterTofu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HarmonyLib;
+using UnityEngine;
+
+namespace NoWaterTofu
+{
+    [HarmonyPatch(typeof(MicrobeMusherConfig), "ConfigureBuildingTemplate")]
+    public class NoWaterTofu_Patch : KMod.UserMod2
+    {
+        static void Postfix(GameObject go, Tag prefab_tag)
+        {
+			ComplexRecipe.RecipeElement[] tofuIngredients = new ComplexRecipe.RecipeElement[1]//[2]
+			{
+			new ComplexRecipe.RecipeElement("BeanPlantSeed", 6f)//,
+			//new ComplexRecipe.RecipeElement("Water".ToTag(), 50f)
+			};//basically just recklessly copy paste the original code from private function configurerecipe and comment out the water
+
+			TofuConfig.recipe.ingredients = tofuIngredients;
+			
+		}
+    }
+}

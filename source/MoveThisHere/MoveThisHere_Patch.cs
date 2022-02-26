88ff79bbb5f51d9ee1d6de0ed36d6649f8417a4a
DoctorFeelGoodMD <82419179+DoctorFeelGoodMD@users.noreply.github.com>
2022-02-26 09:10:48 -0500

    Update

--- source/MoveThisHere/MoveThisHere_Patch.cs
+++ source/MoveThisHere/MoveThisHere_Patch.cs
@@ -19,9 +19,11 @@ namespace MoveThisHere
 			{
 				public static void Prefix()
 				{
-					Utils.AddBuildingStrings(HaulingPointConfig.Id, HaulingPointConfig.DisplayName, HaulingPointConfig.Description, HaulingPointConfig.Effect);
-					Utils.AddBuildingToPlanScreen(HaulingPointConfig.Id, "Base", "StorageLocker");
+					
+					Utils.AddBuildingStrings(HaulingPointConfig.Id, HaulingPointConfig.DisplayName, HaulingPointConfig.Description, HaulingPointConfig.Effect);		
 					// Add haulpoint to build menu with the help of utils below
+
+					Utils.AddPlan("Base", "storage", HaulingPointConfig.Id, "StorageLocker");
 				}
 			}
 
@@ -96,32 +98,50 @@ namespace MoveThisHere
 			Strings.Add($"STRINGS.BUILDINGS.PREFABS.{buildingId.ToUpperInvariant()}.EFFECT", effect);
 		}
 
-		private static PlanScreen.PlanInfo GetMenu(HashedString category)
+		//thanks psyko for new building adding methods
+		public static void AddPlan(HashedString category, string subcategory, string idBuilding, string addAfter = null)
 		{
-			foreach (var menu in TUNING.BUILDINGS.PLANORDER)
+			Debug.Log("Adding " + idBuilding + " to category " + category);
+			foreach (PlanScreen.PlanInfo menu in TUNING.BUILDINGS.PLANORDER)
 			{
-				if (menu.category == category) return menu;
+				if (menu.category == category)
+				{
+					AddPlanToCategory(menu, subcategory, idBuilding, addAfter);
+					return;
+				}
 			}
 
-			throw new System.Exception("The plan menu was not found in TUNING.BUILDINGS.PLANORDER.");
+			Debug.Log($"Unknown build menu category: ${category}");
 		}
 
-		public static void AddBuildingToPlanScreen(string buildingID, HashedString category, string addAferID = null)
+		private static void AddPlanToCategory(PlanScreen.PlanInfo menu, string subcategory, string idBuilding, string addAfter = null)
 		{
-			var categoryMenu = GetMenu(category);
-			if (categoryMenu.data is List<string> buildings)
+			List<KeyValuePair<string, string>> data = menu.buildingAndSubcategoryData;
+			if (data != null)
 			{
-				if (addAferID != null)
+				if (addAfter == null)
 				{
-					var i = buildings.IndexOf(addAferID);
-					if (i == -1 || i == buildings.Count - 1) buildings.Add(buildingID);
-					else buildings.Insert(i + 1, buildingID);
+					data.Add(new KeyValuePair<string, string>(idBuilding, subcategory));
 				}
 				else
 				{
-					buildings.Add(buildingID);
+					/*
+                    foreach (KeyValuePair<string, string> k in data)
+                    {
+                        Debug.Log($"{k.Key}/{k.Value}");
+                    }
+                    */
+					int index = data.IndexOf(new KeyValuePair<string, string>(addAfter, subcategory));
+					if (index == -1)
+					{
+						Debug.Log($"Could not find building {subcategory}/{addAfter} to add {idBuilding} after. Adding at the end !");
+						data.Add(new KeyValuePair<string, string>(idBuilding, subcategory));
+						return;
+					}
+					data.Insert(index + 1, new KeyValuePair<string, string>(idBuilding, subcategory));
 				}
 			}
 		}
+
 	}
 }
